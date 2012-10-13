504963a10e141864379233f782229d7fd401697b
Anders Ljusberg <andlju@gmail.com>
2012-10-13 11:11:14 +0200

    Starting point

--- src/QuickVoter/Controllers/QuestionsController.cs
+++ src/QuickVoter/Controllers/QuestionsController.cs
@@ -57,9 +57,6 @@ namespace QuickVoter.Controllers
             _context.Questions.Add(question);
             _context.SaveChanges();
 
-            var hub = GlobalHost.ConnectionManager.GetHubContext<QuestionHub>();
-            hub.Clients.questionAdded(question);
-
             return question;
         }
         
@@ -84,9 +81,6 @@ namespace QuickVoter.Controllers
             
             _context.SaveChanges();
 
-            var hub = GlobalHost.ConnectionManager.GetHubContext<QuestionHub>();
-            hub.Clients["Question" + questionId].answerAdded(newAnswer);
-
             return newAnswer;
         }
 
@@ -98,9 +92,6 @@ namespace QuickVoter.Controllers
             answer.Votes++;
             
             _context.SaveChanges();
-
-            var hub = GlobalHost.ConnectionManager.GetHubContext<QuestionHub>();
-            hub.Clients["Question" + questionId].answerUpdated(answer);
 
             return answer;
         }
